507072e1d97fcc77a41293ff1d8be80424a95499
Aaron Wu <demonaarwu@gmail.com>
2024-08-23 12:20:32 +0800

    Updates

--- src/utils/Row.js
+++
@@ -1,5 +0,0 @@
-export default function Row(props) {
-  return (
-    <div className="row" {...props}>{props.children}</div>
-  )
-}
--- src/utils/Scroll.js
+++
@@ -1,9 +0,0 @@
-import $ from 'jquery';
-
-export default function Scroll(props) {
-  return (
-    <span style={{ cursor: "pointer" }} onClick={() => {
-      $(props.to)[0].scrollIntoView();
-    }} {...props}>{props.children}</span>
-  );
-}
