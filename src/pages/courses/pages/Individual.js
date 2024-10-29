35373dcfc0801fdfc691e78dc135e2c9ec8e630e
Aaron Wu <demonaarwu@gmail.com>
2024-10-29 14:57:32 +0800

    Change Course information

--- src/pages/courses/pages/Individual.js
+++ src/pages/courses/pages/Individual.js
@@ -21,153 +21,57 @@ function setContent(index, view, textContent, setText, setSlides) {
       "web": "網頁",
       "python": "Python",
       "game_design": "遊戲設計",
-      "blender": "Blender",
       "algorithm": "演算法",
-      "js_and_ts": "JS & TS",
-      "python-2": "進階 Python",
-      "game_develop": "遊戲開發",
-      "algorithm-2": "進階演算法"
+      "linux": "資安",
     },
     imgSrcs = {
       "web":          "/images/courses/web.avif",
       "python":       "/images/courses/python.jpg",
       "game_design":  "/images/courses/game_design.webp",
-      "blender":      "/images/courses/blender.jpg",
       "algorithm":    "/images/courses/algorithm.jpg",
-      "js_and_ts":    "/images/courses/js_and_ts.jpg",
-      "python-2":     "/images/courses/python-2.jpg",
-      "game_develop": "/images/courses/game_develop.png",
-      "algorithm-2":  "/images/courses/algorithm-2.webp"
+      "linux":    "/images/courses/linux.avif",
     },
     slides = {
       "web": <>
-        <h4 id="first">網頁簡介 & HTML 1</h4>
-        <Slides url="https://slides.com/sixbro/desk/" />
-        <h4 id="second">CSS</h4>
-        <Slides url="https://slides.com/sixbro/web-2/" />
-        <h4 id="third">Bootstrap 1</h4>
-        <Slides url="https://slides.com/sixbro/web-3/" />
-        <h4 id="fourth">Bootstrap 2 - RWD</h4>
-        <Slides url="https://slides.com/sixbro/web-4/" />
-        <h4 id="fifth">JavaScript</h4>
-        <Slides url="https://slides.com/star_huey/web_js/" />
-        <h4 id="sixth">Github Pages</h4>
-        <Slides url="https://slides.com/star_huey/github_pages/" />
+        <h4 id="first">網頁基礎</h4>
+        <Slides url="https://slides.com/laura07110717/copy-of" />
       </>,
       "python": <>
-        <h4 id="first">Python 簡介</h4>
-        <Slides url="https://slides.com/d11130110/minimal-ec3a0e/" />
-        <h4 id="second">基礎語法</h4>
-        <Slides url="https://slides.com/d11130110/minimal-5775aa/" />
-        <h4 id="third">基礎語法 2</h4>
-        <Slides url="https://slides.com/d11130110/minimal/" />
-        <h4 id="fourth">Function</h4>
-        <Slides url="https://slides.com/ck1110793/python_function/" />
-        <h4 id="fifth">OOP & Module</h4>
-        <Slides url="https://slides.com/ykchiu/python-oop/fullscreen#/python-5-oop-and-module/" />
-        <h4 id="sixth">File IO</h4>
-        <Slides url="https://slides.com/ck1110793/python_file_io/" />
-        <h4 id="seventh">爬蟲（一）</h4>
-        <Slides url="https://slides.com/ykchiu/python-7-web_crawler_part_i/" />
-        <h4 id="eighth">Selenium</h4>
-        <Slides url="https://slides.com/ck1110793/python_selenium/" />
-        <h4 id="nineth">GUI</h4>
-        <Slides url="https://slides.com/ykchiu/python-gui/" />
-        <h4 id="tenth">PyAutoGUI & Turtle</h4>
-        <Slides url="https://slides.com/ck1110793/python-10/" />
+        <h4 id="first">基礎語法 1</h4>
+        <Slides url="https://slides.com/d11231621/python-1" />
+        <h4 id="second">基礎語法 2</h4>
+        <Slides url="https://slides.com/d11231621/python-2" />
+        <h4 id="third">基礎語法 3</h4>
+        <Slides url="https://slides.com/d11231621/python-3" />
       </>,
       "game_design": <>
-        <h4 id="pico-8">Pico-8</h4>
-        <Slides url="https://slides.com/star_huey/game/" />
-        <h4 id="u0">Unity: 0. Unity基本介紹及初始準備</h4>
-        <Slides url="https://slides.com/mhyang/43s_unity_class-0/" />
-        <h4 id="u12">Unity: 1. 細說遊戲物件 & 2. 讓角色動起來</h4>
-        <Slides url="https://slides.com/mhyang/43s_unity_class-1and2/" />
-        <h4 id="u34">Unity: 3. 基礎物理系統 & 4. 簡易平台遊戲製作</h4>
-        <Slides url="https://slides.com/mhyang/43s_unity_class-3and4/" />
+        <h4 id="first">Unity 第一堂：基本介紹及初始準備</h4>
+        <Slides url="https://slides.com/d11230618/ckefgisc_unity_class1/" />
+        <h4 id="second">Unity 第二堂</h4>
+        <Slides url="https://slides.com/aprilfirst/code" />
+        <h4 id="third">Unity 第三堂</h4>
+        <Slides url="https://slides.com/d11230618/ckefgisc_unity_class3" />
+        <h4 id="fourth">Unity 第四堂：TileMap & random brush</h4>
+        <Slides url="https://slides.com/jellyyfish/unity-tilemap" />
       </>,
-      "blender": <>
-        <h4 id="first">About Blender</h4>
-        <Slides url="https://slides.com/stella-a/deck/" />
-        <h4 id="second">渲染、材質 & 粒子</h4>
-        <Slides url="https://slides.com/ck1110835/blender02/" />
-        <h4 id="third">隨機材質 & 貼圖</h4>
-        <Slides url="https://slides.com/stella-a/deck-f81f94/" />
-        <h4 id="fourth">杯子</h4>
-        <Slides url="https://slides.com/ck1110835/blender/" />
-        <h4 id="fifth">方形小屋</h4>
-        <Slides url="https://slides.com/stella-a/deck-4a3712/" />
-        <h4 id="sixth">方形小屋 (1)</h4>
-        <Slides url="https://slides.com/stella-a/deck-bd56c3/" />
-        <h4 id="seventh">方形小屋 (2)</h4>
-        <Slides url="https://slides.com/d11131634/deck/" />
-        <h4 id="eighth">方形小屋 (3)</h4>
-        <Slides url="https://slides.com/d11131634/deck-573baa/" />
-        <h4 id="nineth">方形小屋 (4)</h4>
-        <Slides url="https://slides.com/d11131634/deck-6ae291/" />
-        <h4 id="tenth">方形小屋 (5)</h4>
-        <Slides url="https://slides.com/d11131634/deck-8cfa5c/" />
+      "linux": <>
+        <h4 id="first">資安第一堂：Linux 介紹和 Shell</h4>
+        <Slides url="https://slides.com/demonaarwu/linux-0/" />
+        <h4 id="second">資安第二堂：VM & Python</h4>
+        <Slides url="https://slides.com/demonaarwu/linux-1" />
+        <h4 id="third">資安第三堂：Git & GitHub 入門</h4>
+        <Slides url="https://slides.com/demonaarwu/guide_to_git_and_github" />
       </>,
       "algorithm": <>
-        <h4 id="first">C++ 語法 1</h4>
-        <Slides url="https://slides.com/ganyumywife/algo-0/" />
-        <h4 id="second">C++ 語法 2</h4>
-        <Slides url="https://slides.com/cjtsai/algo-1/" />
-        <h4 id="third">C++ 語法 3 - 複雜度 & 一些 STL</h4>
-        <Slides url="https://slides.com/ckeisc807/algo2/" />
-        <h4 id="fourth">C++ 語法 4 - 更多 STL</h4>
-        <Slides url="https://slides.com/ganyumywife/algo-3/" />
-        <h4 id="fifth">演算法 1 - 枚舉、貪婪、二分搜 & 分治</h4>
-        <Slides url="https://slides.com/cjtsai/algo-basic/" />
-        <h4 id="sixth">DP</h4>
-        <Slides url="https://slides.com/ckeisc807/dynamic-programming0/" />
-        <h4 id="seventh">圖論（一）</h4>
-        <Slides url="https://slides.com/sea_of_voices/graph-theory0/" />
-        <h4 id="eighth">圖論（二）</h4>
-        <Slides url="https://slides.com/sea_of_voices/graph-theory1/" />
-        <h4 id="tenth">小社賽題解</h4>
-        <Slides url="https://slides.com/ckefgisc28th/contest/" />
+        <h4 id="first">C++ 基礎語法之一</h4>
+        <Slides url="https://slides.com/keaucucal/c" />
+        <h4 id="second">C++ 基礎語法之二</h4>
+        <Slides url="https://slides.com/wenian/cpp-1/" />
+        <h4 id="third">C++ STL 之一</h4>
+        <Slides url="https://slides.com/oct0920/template/" />
+        <h4 id="fourth">C++ STL 之二</h4>
+        <Slides url="https://slides.com/oct0920/copy-of-template-209f73" />
       </>,
-      "js_and_ts": <>
-        <h4 id="all">全部</h4>
-        <Slides url="https://slides.com/star_huey/js_and_ts/" />      
-      </>,
-      "python-2": <>
-        <h4 id="first">OpenCV</h4>
-        <Slides url="https://slides.com/d11130110/minimal-458a40/" />
-        <h4 id="second">PyVista (上)</h4>
-        <Slides url="https://slides.com/ykvonckhs/pyvista/" />
-        <h4 id="third">PyVista (下)</h4>
-        <Slides url="https://slides.com/ykvonckhs/pyvista2/" />
-      </>,
-      "game_develop": <>
-        <h4 id="u56">Unity 5. Tilemap & 6. Prefab</h4>
-        <Slides url="https://slides.com/mhyang/unity-5and6/" />
-        <h4 id="u7">Unity 7. 淺談 AI 尋路及延伸</h4>
-        <Slides url="https://slides.com/mhyang/unity-7/" />
-        <h4 id="u89">Unity 8. 實用 UI & 9. 場景切換與管理</h4>
-        <Slides url="https://slides.com/mhyang/unity-8and9/" />
-      </>,
-      "algorithm-2": <>
-        <h4 id="first">線段樹</h4>
-        <Slides url="https://slides.com/ckeisc807/segment_tree/" />
-        <h4 id="second">資料結構</h4>
-        <Slides url="https://slides.com/sea_of_voices/data_structure_new/" />
-        <h4 id="third">圖論（三）</h4>
-        <Slides url="https://slides.com/sea_of_voices/graph-theory2/" />
-        <h4 id="fourth">DP（二）</h4>
-        <Slides url="https://slides.com/ckeisc807/copy-of-dynamic-programming/" />
-        <h4 id="fifth">樹論</h4>
-        <Slides url="https://slides.com/cjtsai/tree/" />
-        <h4 id="sixth">計算幾何</h4>
-        <Slides url="https://slides.com/aaw/geometry-15/" />
-        <h4 id="seventh">字串</h4>
-        <Slides url="https://slides.com/cjtsai/string" />
-        <h4 id="eighth">數論</h4>
-        <Slides url="https://slides.com/lemonilemon/copy-of" />
-        <h4 id="ninth">補坑</h4>
-        <Slides url="https://slides.com/cjtsai/misc" />
-      </>
     }
 
   setText(<CourseDetails
@@ -210,7 +114,7 @@ function slidesContentsOnScroll(ev) {
 export default function CourseIndividual() {
   const { id: courseId } = useParams();
 
-  const index = [ "web", "python", "game_design", "blender", "algorithm", "js_and_ts", "python-2", "game_develop", null, "algorithm-2" ].indexOf(courseId);
+  const index = [ "algorithm", "game_design", "linux", "python", "web"].indexOf(courseId);
 
   const [text, setText] = React.useState();
   const [slides, setSlides] = React.useState();
--- src/pages/courses/pages/List.js
+++ src/pages/courses/pages/List.js
@@ -26,43 +26,43 @@ export default function CourseList() {
       課程介紹
     </section>
     <section id="courses">
-      <SectionTitle>112 上</SectionTitle>
+      <SectionTitle>113 上</SectionTitle>
       <div className="container py-1">
         {text ? (<>
           <CourseIntro
-            title="網頁"
-            imgSrc="/images/courses/web.avif"
+            title="演算法 (C++)"
+            imgSrc="/images/courses/algorithm.jpg"
             content={text.split("<%-== next ==-%>")[0]}
             btnColor="crimson"
-            href="/web/"
+            href="/algorithm/"
+          />
+          <CourseIntro
+            title="遊戲設計 (Unity)"
+            imgSrc="/images/courses/game_design.webp"
+            content={text.split("<%-== next ==-%>")[1]}
+            btnColor="gamboge"
+            href="/game_design/"
+          />
+          <CourseIntro
+            title="資安 (Linux)"
+            imgSrc="/images/courses/linux.avif"
+            content={text.split("<%-== next ==-%>")[2]}
+            btnColor="success"
+            href="/linux/"
           />
           <CourseIntro
             title="Python"
             imgSrc="/images/courses/python.jpg"
-            content={text.split("<%-== next ==-%>")[1]}
-            btnColor="gamboge"
+            content={text.split("<%-== next ==-%>")[3]}
+            btnColor="iris"
             href="/python/"
           />
           <CourseIntro
-            title="遊戲設計"
-            imgSrc="/images/courses/game_design.webp"
-            content={text.split("<%-== next ==-%>")[2]}
-            btnColor="success"
-            href="/game_design/"
-          />
-          <CourseIntro
-            title="Blender"
-            imgSrc="/images/courses/blender.jpg"
-            content={text.split("<%-== next ==-%>")[3]}
-            btnColor="iris"
-            href="/blender/"
-          />
-          <CourseIntro
-            title="演算法"
-            imgSrc="/images/courses/algorithm.jpg"
+            title="網頁"
+            imgSrc="/images/courses/web.avif"
             content={text.split("<%-== next ==-%>")[4]}
             btnColor="orchid"
-            href="/algorithm/"
+            href="/web/"
           />
         </>) : (<>
           <Loading />
@@ -70,47 +70,6 @@ export default function CourseList() {
         </>)}
       </div>
 
-      <SectionTitle>112 下</SectionTitle>
-      <div className="container py-1">
-        {text ? (<>
-          <CourseIntro
-            title="JS & TS"
-            imgSrc="/images/courses/js_and_ts.jpg"
-            content={text.split("<%-== next ==-%>")[5]}
-            btnColor="crimson"
-            href="/js_and_ts/"
-          />
-          <CourseIntro
-            title="進階 Python"
-            imgSrc="/images/courses/python-2.jpg"
-            content={text.split("<%-== next ==-%>")[6]}
-            btnColor="gamboge"
-            href="/python-2/"
-          />
-          <CourseIntro
-            title="遊戲開發"
-            imgSrc="/images/courses/game_develop.png"
-            content={text.split("<%-== next ==-%>")[7]}
-            btnColor="success"
-            href="/game_develop/"
-          />
-          <CourseIntro
-            title="彈性時間"
-            imgSrc="/images/courses/tea_time.jpg"
-            content={text.split("<%-== next ==-%>")[8]}
-          />
-          <CourseIntro
-            title="進階演算法"
-            imgSrc="/images/courses/algorithm-2.webp"
-            content={text.split("<%-== next ==-%>")[9]}
-            btnColor="orchid"
-            href="/algorithm-2/"
-          />
-        </>) : (<>
-          <Loading />
-          <div style={{ height: "100dvh" }}></div>
-        </>)}
-      </div>
     </section>
   </>);
 }
